3d2fae7262e8584744cfff4a1efadb2589f59189
Miguel-Leite <miguelleite200leite@gmail.com>
2022-01-11 16:09:02 +0100

    method to create user

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index() {
-        return view('home.panel');
+
+        $users = User::all();
+
+        return view('home.panel',compact('users'));
     }
 
     public function phone() {
@@ -16,5 +20,27 @@ class DashboardController extends Controller
 
     public function guardianshipTransfer() {
         return view('home.guardianshipTransfer');
+    }
+
+    public function createUser(Request $request) {
+        
+        $user = new User();
+
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = $request->password;
+
+        $verify = $user->save();
+
+        if ($verify) {
+            return redirect()
+                    ->route('dashboard.index')
+                    ->with('succes','Usuario adicionado com sucesso!');
+        }
+
+        return redirect()
+                    ->route('dashboard.index')
+                    ->with('danger','Ops! Error ao adicionar usuario.');
+
     }
 }
