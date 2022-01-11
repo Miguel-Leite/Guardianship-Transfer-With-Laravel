cdd67d2706e61d8b58b72b9319fdb8f0f5a233f0
Miguel-Leite <miguelleite200leite@gmail.com>
2022-01-11 16:08:22 +0100

    migrate

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function phones() {
+        return $this->hashMany('App\Models\Phone');
+    }
 }
