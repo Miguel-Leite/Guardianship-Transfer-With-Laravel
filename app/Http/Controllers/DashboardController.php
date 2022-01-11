fef9d494554c1fc2b53d7d390c90935bfa279656
Miguel-Leite <miguelleite200leite@gmail.com>
2022-01-11 19:25:17 +0100

    add authentication

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,20 +2,59 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Phone;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
+
+    public function __construct() {
+        $this->middleware('auth');
+    }
+
     public function index() {
 
+        $logger = Auth::user();
         $users = User::all();
 
-        return view('home.panel',compact('users'));
+        return view('home.panel',compact('users','logger'));
+    }
+
+    public function signIn() {
+        if (Auth::check()) {
+            return redirect()->back();
+        }
+
+        return view('auth.index');
+    }
+
+    public function auth(Request $request) {
+        
+        $user = Auth::user();
+        
+
+        $this->validate($request,[
+            'email'=>'required',
+            'password'=>'required',
+        ],[
+            'email.required'=>'Por favor informe o seu e-mail!',
+            'password.required'=>'Por favor informe a senha!'
+        ]);
+
+        if (Auth::attempt(['email' => $request->email,'password'=>$request->password])) {
+            return redirect()->route('dashboard.index')->with('success',"Bem-vindo ao panel de control {$user->name}");
+        } else {
+            return redirect()->back()->with('danger','E-mail ou senha invalida!');
+        }
     }
 
     public function phone() {
-        return view('home.phone');
+
+        $users = User::all();
+
+        return view('home.phone',compact('users'));
     }
 
     public function guardianshipTransfer() {
@@ -23,24 +62,37 @@ class DashboardController extends Controller
     }
 
     public function createUser(Request $request) {
-        
-        $user = new User();
 
-        $user->name = $request->name;
-        $user->email = $request->email;
-        $user->password = $request->password;
-
-        $verify = $user->save();
+        $verify = User::create([
+            'email' => $request->email, 
+            'name' => $request->name, 
+            'phone' => $request->phone, 
+            'password' => bcrypt($request->password)]);
 
         if ($verify) {
             return redirect()
                     ->route('dashboard.index')
-                    ->with('succes','Usuario adicionado com sucesso!');
+                    ->with('success','Usuario adicionado com sucesso!');
         }
 
         return redirect()
                     ->route('dashboard.index')
                     ->with('danger','Ops! Error ao adicionar usuario.');
 
+    }
+
+    public function deleteUser($id) {
+
+        $user = User::find($id);
+        $user->delete();
+
+        return redirect()
+                    ->route('dashboard.index')
+                    ->with('success','Registro apagado com sucesso!');
+    }
+
+    public function addPhone(Request $request) {
+
+        $phone = new Phone();
     }
 }
